684888e898cb02a6103c640feea16b26e92f822e
prasannapaudyal <prsnn.paudyal@gmail.com>
2018-11-26 12:50:20 -0700

    search for substring

---
+++ search.go
@@ -0,0 +1,14 @@
+/*
+* returns true if the substring contains in the string.
+* false otherwise.
+*/
+package main
+
+import "fmt"
+import "strings"
+
+func main(){
+    s:= "boise state university"
+    fmt.Println(strings.Contains(s,"tat"))
+
+}
